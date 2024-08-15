0617147259b14da45c4947e9a106def41950925d
micaela prado <pradomicaela99@gmail.com>
2024-08-15 13:26:33 -0500

    estilos con css

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from '../ItemDetail/ItemDetail';
+import "./ItemDetailContainer.css"
 
 import cremaHumectanteImg from '../../assets/crema-humectante.jpg';
 import locionFacialImg from '../../assets/locion-facial.jpg';
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./ItemList.css"
 
 const ItemList = () => {
   return (
