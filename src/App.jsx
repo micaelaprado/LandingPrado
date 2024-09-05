7ffe53338aa465b64a5271f0e93e33470ef67618
micaela prado <pradomicaela99@gmail.com>
2024-09-05 13:00:18 -0500

    firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import AppRouter from './AppRouter';
+import "App.css";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import firebase from 'firebase/compat/app';
 
 function App() {
+  const db = getFirestore(firebaseApp);
+
   return <AppRouter />;
 }
 
